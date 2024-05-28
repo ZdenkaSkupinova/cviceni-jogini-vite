735aa234e30c98702abce69892a1810d4773d965
unknown <zdenka.x@post.cz>
2024-05-28 13:47:54 +0200

    update

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,13 +6,17 @@ import { Header } from '../components/header/header';
 import { Intro } from '../components/intro/intro';
 import { Pose } from '../components/pose/pose';
 
+const text = "Naše lekce jsou zaměřeny na potřeby klientů, kteří mají odvahu zkusit něco nového. Cvičíme v pomalém tempu s podrobným slovním doprovodem. Postupně se seznámíte se základními principy jógy, jak přístupovat k sobě i ostatním. Krok za krokem objevíte a dostanete pod kontrolu svoje tělo, pocity a emoce."
+const title = 'Jogíni'
+const heading = 'Vítejte mezi Jogíny'
+
 
 document.querySelector('#root').innerHTML = render(
   <div id="app" className="app container">
       <>
-      <Header title = 'Jogíni'/>
-      <Intro heading = 'Vítejte mezi Jogíny'
-             text = "Naše lekce jsou zaměřeny na potřeby klientů, kteří mají odvahu zkusit něco nového. Cvičíme v pomalém tempu s podrobným slovním doprovodem. Postupně se seznámíte se základními principy jógy, jak přístupovat k sobě i ostatním. Krok za krokem objevíte a dostanete pod kontrolu svoje tělo, pocity a emoce."/>
+      <Header title = {title}/>
+      <Intro heading = {heading}
+             text = {text} />
       <Pose />
       </>
   </div>
